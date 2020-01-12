bd83252045d28db0bfb2e107ca922e5866ddf3a7
francofresno <franco.fresno@gmail.com>
2020-01-12 19:19:54 -0300

    first commit

---
+++ src/app/pages/graphics1/graphics1.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-graphics1',
+  template: `
+    <p>
+      graphics1 works!
+    </p>
+  `,
+  styles: []
+})
+export class Graphics1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
